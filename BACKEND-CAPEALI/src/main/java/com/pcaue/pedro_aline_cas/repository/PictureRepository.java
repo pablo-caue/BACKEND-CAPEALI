cf462789e58f6a41c2d7647101718553027da517
Pablo Cauê <90734548+pablo-caue@users.noreply.github.com>
2024-11-07 06:57:46 -0300

    Delete BACKEND-CAPEALI directory

--- BACKEND-CAPEALI/src/main/java/com/pcaue/pedro_aline_cas/repository/PictureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pcaue.pedro_aline_cas.repository;
-
-import com.pcaue.pedro_aline_cas.model.Picture;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PictureRepository extends MongoRepository<Picture, String> {
-}
